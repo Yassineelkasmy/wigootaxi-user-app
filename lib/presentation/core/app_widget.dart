d54fdf422dcfd73617877fbd0b64f4ff17777844
yassineelkasmy <yassineelkasmy@gmail.com>
2022-04-10 04:55:37 +0000

    Refactor app theming constants

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,9 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:taxidriver/presentation/theme/theme.dart';
 import '../routes/router.gr.dart';
 
 class AppWidget extends HookConsumerWidget {
@@ -18,77 +17,9 @@ class AppWidget extends HookConsumerWidget {
       useInheritedMediaQuery: true,
       routeInformationParser: _appRouter.defaultRouteParser(),
       routerDelegate: _appRouter.delegate(),
-      title: "WIGO TAXI",
-      theme: FlexThemeData.light(
-        scheme: FlexScheme.material,
-        fontFamily: GoogleFonts.montserrat().fontFamily,
-        blendLevel: 18,
-        appBarStyle: FlexAppBarStyle.primary,
-        appBarOpacity: 0.95,
-        appBarElevation: 0,
-        transparentStatusBar: true,
-        tabBarStyle: FlexTabBarStyle.forAppBar,
-        tooltipsMatchBackground: true,
-        swapColors: false,
-        lightIsWhite: false,
-        useSubThemes: true,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        subThemesData: const FlexSubThemesData(
-          useTextTheme: true,
-          fabUseShape: true,
-          interactionEffects: true,
-          bottomNavigationBarElevation: 0,
-          bottomNavigationBarOpacity: 0.95,
-          navigationBarOpacity: 0.95,
-          navigationBarMutedUnselectedText: true,
-          navigationBarMutedUnselectedIcon: true,
-          inputDecoratorIsFilled: true,
-          inputDecoratorBorderType: FlexInputBorderType.outline,
-          inputDecoratorUnfocusedHasBorder: true,
-          inputDecoratorFillColor: Colors.white,
-          blendOnColors: true,
-          blendTextTheme: true,
-          popupMenuOpacity: 0.95,
-        ),
-      ),
-      darkTheme: FlexThemeData.dark(
-        scheme: FlexScheme.material,
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 18,
-        appBarStyle: FlexAppBarStyle.background,
-        appBarOpacity: 0.95,
-        appBarElevation: 0,
-        transparentStatusBar: true,
-        tabBarStyle: FlexTabBarStyle.forAppBar,
-        tooltipsMatchBackground: true,
-        swapColors: false,
-        darkIsTrueBlack: false,
-        useSubThemes: true,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        fontFamily: GoogleFonts.montserrat().fontFamily,
-        subThemesData: const FlexSubThemesData(
-          useTextTheme: true,
-          fabUseShape: true,
-          interactionEffects: true,
-          bottomNavigationBarElevation: 0,
-          bottomNavigationBarOpacity: 0.95,
-          navigationBarOpacity: 0.95,
-          navigationBarMutedUnselectedText: true,
-          navigationBarMutedUnselectedIcon: true,
-          inputDecoratorIsFilled: true,
-          inputDecoratorBorderType: FlexInputBorderType.outline,
-          inputDecoratorUnfocusedHasBorder: true,
-          blendOnColors: true,
-          blendTextTheme: true,
-          popupMenuOpacity: 0.95,
-        ),
-      ).copyWith(
-        textTheme:
-            GoogleFonts.rubikBeastlyTextTheme(ThemeData.light().textTheme),
-        inputDecorationTheme: const InputDecorationTheme(
-          fillColor: Colors.white,
-        ),
-      ),
+      title: "WIGOO TAXI",
+      theme: lightTheme,
+      darkTheme: darkTheme,
       debugShowCheckedModeBanner: false,
     );
   }
