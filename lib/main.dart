79eb143333234790ae4688189a52a50588495a82
yassineelkasmy <yassineelkasmy@gmail.com>
2022-04-10 04:50:27 +0000

    add flutterfire configuration CLI only for android

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:taxidriver/firebase_options.dart';
 import 'package:taxidriver/presentation/core/app_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ProviderScope(
       child: EasyLocalization(
