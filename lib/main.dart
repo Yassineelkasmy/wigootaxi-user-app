e76e638ae6806c0e72076d323fb4631600ca543c
yassineelkasmy <yassineelkasmy@gmail.com>
2022-07-22 20:23:53 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:in_app_notification/in_app_notification.dart';
@@ -9,9 +11,16 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     ProviderScope(
       child: InAppNotification(child: AppWidget()),
     ),
   );
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  final user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    await firebaseMessaging.subscribeToTopic(user.uid);
+    firebaseMessaging.requestPermission(alert: true, badge: true, sound: true);
+  }
 }
