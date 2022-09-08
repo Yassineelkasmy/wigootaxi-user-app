f01c911e65853e18e2588ea2610323c2d18c5a4e
yassineelkasmy <yassineelkasmy@gmail.com>
2022-09-09 00:37:55 +0100

    quick ui fix

--- lib/presentation/shared/in_app_notfication.dart
+++ lib/presentation/shared/in_app_notfication.dart
@@ -12,36 +12,34 @@ class InnerNotifications extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Card(
-        color: isScuccess ? Colors.green : Colors.red.shade400,
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 5,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              10.w.horizontalSpace,
-              Flexible(
-                child: Text(
-                  message,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+    return Card(
+      color: isScuccess ? Colors.green : Colors.red.shade400,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 5,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            10.w.horizontalSpace,
+            Flexible(
+              child: Text(
+                message,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Icon(
-                isScuccess ? Icons.check : Icons.error,
-                size: 32,
-                color: Colors.white,
-              ),
-              10.w.horizontalSpace,
-            ],
-          ),
+            ),
+            Icon(
+              isScuccess ? Icons.check : Icons.error,
+              size: 32,
+              color: Colors.white,
+            ),
+            10.w.horizontalSpace,
+          ],
         ),
       ),
     );
