19230cf1a4634425e5308406a9086f5dfbe73765
yassineelkasmy <yassineelkasmy@gmail.com>
2022-10-18 20:28:50 +0100

    update

--- lib/presentation/home/pick_location/activate_location_or_map_page.dart
+++ lib/presentation/home/pick_location/activate_location_or_map_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,6 +11,7 @@ import 'package:taxidriver/presentation/home/pick_location/location_map.dart';
 import 'package:taxidriver/presentation/shared/submit_button.dart';
 import 'package:taxidriver/presentation/theme/spacings.dart';
 import 'package:taxidriver/shared/ui/map_animation.dart';
+import 'package:upgrader/upgrader.dart';
 
 class ActivateLocationOrMapPage extends HookConsumerWidget {
   const ActivateLocationOrMapPage({Key? key}) : super(key: key);
@@ -21,83 +24,93 @@ class ActivateLocationOrMapPage extends HookConsumerWidget {
     final pickUpState = ref.watch(pickUpProvider);
 
     return Scaffold(
-      body: locationState.position != null
-          ? SizedBox(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Stack(
-                      fit: StackFit.passthrough,
-                      children: [
-                        Positioned.fill(
-                          bottom: .3.sh,
-                          child: LocationMap(
-                            lat: locationState.position!.latitude,
-                            long: locationState.position!.longitude,
-                            onCameraIdle: () {
-                              if (!pickUpState.pickUpChosen ||
-                                  !pickUpState.dropOffChosen) {
+      body: UpgradeAlert(
+        upgrader: Upgrader(
+          showIgnore: false,
+          durationUntilAlertAgain: const Duration(minutes: 5),
+          dialogStyle: Platform.isIOS
+              ? UpgradeDialogStyle.cupertino
+              : UpgradeDialogStyle.material,
+        ),
+        child: locationState.position != null
+            ? SizedBox(
+                height: double.maxFinite,
+                width: double.maxFinite,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Stack(
+                        fit: StackFit.passthrough,
+                        children: [
+                          Positioned.fill(
+                            bottom: .3.sh,
+                            child: LocationMap(
+                              lat: locationState.position!.latitude,
+                              long: locationState.position!.longitude,
+                              onCameraIdle: () {
+                                if (!pickUpState.pickUpChosen ||
+                                    !pickUpState.dropOffChosen) {
+                                  pickUpController.mapEventToState(
+                                    PickUpEvent
+                                        .reverseGecodingFromMapRequested(),
+                                  );
+                                }
+                              },
+                              onCameraMove: (cameraPosition) {
                                 pickUpController.mapEventToState(
-                                  PickUpEvent.reverseGecodingFromMapRequested(),
+                                  PickUpEvent.cameraMoved(
+                                    cameraPosition.target.latitude,
+                                    cameraPosition.target.longitude,
+                                  ),
                                 );
-                              }
-                            },
-                            onCameraMove: (cameraPosition) {
-                              pickUpController.mapEventToState(
-                                PickUpEvent.cameraMoved(
-                                  cameraPosition.target.latitude,
-                                  cameraPosition.target.longitude,
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ))
+            : SizedBox(
+                height: double.maxFinite,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.w,
+                    vertical: 20.h,
+                  ),
+                  child: Column(
+                    children: [
+                      40.h.verticalSpace,
+                      Text(
+                        "Pour une meilleure expérience, donnez-nous accès à votre position actuelle via votre GPS mobile",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                        ),
+                      ),
+                      20.h.verticalSpace,
+                      SizedBox(
+                        width: double.maxFinite,
+                        child: Padding(
+                          padding: kPadding,
+                          child: SubmitButton(
+                            onPressed: () {
+                              locationController.mapEventToState(
+                                LocationEvent.locationRequested(
+                                  pressed: true,
                                 ),
                               );
                             },
+                            text: "Autoriser l'accès au GPS",
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      MapAnimation(),
+                    ],
                   ),
-                ],
-              ))
-          : SizedBox(
-              height: double.maxFinite,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20.w,
-                  vertical: 20.h,
-                ),
-                child: Column(
-                  children: [
-                    40.h.verticalSpace,
-                    Text(
-                      "Pour une meilleure expérience, donnez-nous accès à votre position actuelle via votre GPS mobile",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16.sp,
-                      ),
-                    ),
-                    20.h.verticalSpace,
-                    SizedBox(
-                      width: double.maxFinite,
-                      child: Padding(
-                        padding: kPadding,
-                        child: SubmitButton(
-                          onPressed: () {
-                            locationController.mapEventToState(
-                              LocationEvent.locationRequested(
-                                pressed: true,
-                              ),
-                            );
-                          },
-                          text: "Autoriser l'accès au GPS",
-                        ),
-                      ),
-                    ),
-                    MapAnimation(),
-                  ],
                 ),
               ),
-            ),
+      ),
     );
   }
 }
